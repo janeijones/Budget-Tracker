1b1c426c33d48bc7807d08056fcdd985fe96f96a
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-27 09:37:47 -0500

    sync IndexedDB to upgraded version

--- public/db.js
+++ public/db.js
@@ -3,9 +3,19 @@ let db;
 let budgetVersion;
 
 //initialize IndexedDB
-const request = indexedDB.open('budget_db', 25 || 21);
+const request = indexedDB.open('budget_db', budgetVersion || 21);
 
 //sync db to upgraded version
+
+request.onupgradeneeded = (e) => {
+    db = e.target.result;
+
+    if (db.objectStoreNames.length === 0) { //if object store is empty, create one
+        db.createObjectStore('budgetStore', { autoIncrement: true })
+    }
+};
+
+
 
 
 //check Database then do transactions 
